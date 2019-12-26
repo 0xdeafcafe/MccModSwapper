c6669d1438f89de01475e3a0b953d42536667a56
Alexander Forbes-Reed <git@alx.red>
2019-12-26 20:45:40 +0000

    Added logging

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Windows.Forms;
 
 namespace MccModSwapper
 {
-	static class Program
+	public class Program
 	{
+		public static ILogger<Program> Logger { get; private set; }
+
 		/// <summary>
 		///  The main entry point for the application.
 		/// </summary>
@@ -15,6 +19,16 @@ namespace MccModSwapper
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Main());
+
+			var loggerFactory = new LoggerFactory();
+			var loggerConfig = new LoggerConfiguration()
+				.WriteTo.Console()
+				.WriteTo.File("logs.txt", rollingInterval: RollingInterval.Hour)
+				.CreateLogger();
+
+			loggerFactory.AddSerilog(loggerConfig);
+
+			Logger = loggerFactory.CreateLogger<Program>();
 		}
 	}
 }
