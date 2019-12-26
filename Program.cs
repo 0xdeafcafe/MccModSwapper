436f50d372b344dd607ebbd56c5100aa57392c4a
Alexander Forbes-Reed <git@alx.red>
2019-12-26 21:40:43 +0000

    Fixed logging

--- Program.cs
+++ Program.cs
@@ -15,20 +15,20 @@ namespace MccModSwapper
 		[STAThread]
 		static void Main()
 		{
-			Application.SetHighDpiMode(HighDpiMode.SystemAware);
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Main());
-
 			var loggerFactory = new LoggerFactory();
 			var loggerConfig = new LoggerConfiguration()
 				.WriteTo.Console()
-				.WriteTo.File("logs.txt", rollingInterval: RollingInterval.Hour)
+				.WriteTo.File("logs*.txt", rollingInterval: RollingInterval.Day)
 				.CreateLogger();
 
 			loggerFactory.AddSerilog(loggerConfig);
 
 			Logger = loggerFactory.CreateLogger<Program>();
+
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Main());
 		}
 	}
 }
